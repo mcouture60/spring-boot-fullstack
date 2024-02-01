3266357c97ee40dbe6204993b76d0a1478b60e43
marco <marco6001@gmail.com>
2024-02-01 16:31:44 +0100

    Add Options to Create/Update/Delete customers

--- frontend/react/src/services/client.js
+++ frontend/react/src/services/client.js
@@ -7,3 +7,36 @@ export const getCustomers = async () => {
         throw error;
     }
 }
+
+export const saveCustomer = async (customer) => {
+    try{
+        return await axios.post(
+            `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`,
+            customer
+        );
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deleteCustomer = async (id) => {
+    try{
+        return await axios.delete(
+            `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${id}`
+        );
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateCustomer = async (id, update) => {
+    try{
+        return await axios.put(
+            `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${id}`,
+            update
+        );
+    } catch (error) {
+        throw error;
+    }
+}
+
