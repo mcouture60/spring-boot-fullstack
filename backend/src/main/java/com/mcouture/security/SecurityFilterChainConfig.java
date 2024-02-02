78e46a7cdf73cd49af40ce6796d98925221cc013
marco <marco6001@gmail.com>
2024-02-02 13:22:43 +0100

    Allow /ping to have no authentication

--- backend/src/main/java/com/mcouture/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/mcouture/security/SecurityFilterChainConfig.java
@@ -37,6 +37,10 @@ public class SecurityFilterChainConfig {
                                 "/api/v1/customers",
                                 "/api/v1/auth/login")
                         .permitAll()
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/ping")
+                        .permitAll()
                         .anyRequest()
                         .authenticated())
                 .sessionManagement((session) -> session
