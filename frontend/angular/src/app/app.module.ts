462beff59ea3d3cca7c9518b2312f20a32cece20
marco <marco6001@gmail.com>
2024-02-03 18:05:21 +0100

    Generate angular project

---
+++ frontend/angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {InputTextModule} from "primeng/inputtext";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    InputTextModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
